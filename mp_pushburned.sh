036774d6ac4fcfe38606faabb84fa12b674f4715
uncoyote <jeker.git@outlook.de>
2020-07-17 07:54:44 +0200

    renamed *_pushburned.sh

--- mp_pushburned.sh
+++ mp_pushburned.sh
@@ -2,20 +2,20 @@
 
 export LANG=de_CH.UTF-8
 
-rm prj_dok/puml/rendered/*
+rm mp/puml/rendered/*
 
-FILES=prj_dok/puml/src/*
+FILES=mp/puml/src/*
 for f in $FILES
 do
   echo "****************** Processing $f ***********************************************************"
   
   name_suffix=$(echo $f | cut -d/ -f4) # split filepath with delimiter /, and pick 5th element of result array
   name=$(echo $name_suffix | cut -d. -f1) # split filename with delimiter ., and pick first element of result array
-  out=prj_dok/puml/rendered/${name}.png
+  out=mp/puml/rendered/${name}.png
 
   cat $f | docker run --rm -i puml_burner > $out
 done
 
-git add prj_dok/puml/rendered/*
+git add mp/puml/rendered/*
 git commit -m 'Skript: PUML-Diagramme der Projektdoku regeneriert und beigefügt'
 git push
