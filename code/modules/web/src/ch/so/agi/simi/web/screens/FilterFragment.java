de48affc54c20a1d3b479378c576d1cbc7a2c0d9
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2020-08-24 11:58:51 +0200

    Add empty filter fragment

---
+++ code/modules/web/src/ch/so/agi/simi/web/screens/FilterFragment.java
@@ -0,0 +1,10 @@
+package ch.so.agi.simi.web.screens;
+
+import com.haulmont.cuba.gui.screen.ScreenFragment;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("simi_FilterFragment")
+@UiDescriptor("filter-fragment.xml")
+public class FilterFragment extends ScreenFragment {
+}
