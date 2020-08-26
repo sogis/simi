09ccc9045547712e2053755260c1bd767976598d
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2020-08-26 16:05:51 +0200

    Add browse and edit screen for PubScope

---
+++ code/modules/web/src/ch/so/agi/simi/web/screens/dataproduct_pubscope/DataProduct_PubScopeBrowse.java
@@ -0,0 +1,11 @@
+package ch.so.agi.simi.web.screens.dataproduct_pubscope;
+
+import com.haulmont.cuba.gui.screen.*;
+import ch.so.agi.simi.entity.DataProduct_PubScope;
+
+@UiController("simi_DataProduct_PubScope.browse")
+@UiDescriptor("data-product_pub-scope-browse.xml")
+@LookupComponent("dataProduct_PubScopesTable")
+@LoadDataBeforeShow
+public class DataProduct_PubScopeBrowse extends StandardLookup<DataProduct_PubScope> {
+}
