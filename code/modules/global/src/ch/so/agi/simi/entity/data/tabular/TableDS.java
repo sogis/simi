e89cb17f87bd22e45fdb5f43cfef96965b216e4a
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2020-09-08 10:16:48 +0200

    Remove DataSet interface

--- code/modules/global/src/ch/so/agi/simi/entity/data/tabular/TableDS.java
+++ code/modules/global/src/ch/so/agi/simi/entity/data/tabular/TableDS.java
@@ -1,6 +1,7 @@
 package ch.so.agi.simi.entity.data.tabular;
 
-import ch.so.agi.simi.entity.data.DataSet;
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
 
 import javax.persistence.*;
 
@@ -8,11 +9,24 @@ import javax.persistence.*;
 @Entity(name = "simi_TableDS")
 @DiscriminatorColumn(name = "DTYPE", discriminatorType = DiscriminatorType.STRING)
 @Inheritance(strategy = InheritanceType.JOINED)
-public class TableDS extends DataSet {
+@NamePattern("%s|tableName")
+public class TableDS extends StandardEntity {
     private static final long serialVersionUID = -2643498680251164635L;
 
     @Column(name = "TABLE_NAME", length = 100)
     private String tableName;
+
+    @Lob
+    @Column(name = "REMARKS")
+    private String remarks;
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
 
     public String getTableName() {
         return tableName;
