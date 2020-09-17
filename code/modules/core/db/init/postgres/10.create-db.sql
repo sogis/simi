5d82274a6d85e10788cff9b41411b634525744bd
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2020-09-17 12:26:08 +0200

    Update sql scripts remove FeatureInfo experiments

--- code/modules/core/db/init/postgres/10.create-db.sql
+++ code/modules/core/db/init/postgres/10.create-db.sql
@@ -74,24 +74,6 @@ create table SIMIDATA_TABLE_DS (
     primary key (ID)
 )^
 -- end SIMIDATA_TABLE_DS
--- begin FI_LAYER_RELATION
-create table FI_LAYER_RELATION (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    REL_TYPE varchar(50) not null,
-    DATA_SET_VIEW_ID uuid not null,
-    FEATURE_INFO_ID uuid not null,
-    --
-    primary key (ID)
-)^
--- end FI_LAYER_RELATION
 -- begin SIMIDATA_RASTER_DS
 create table SIMIDATA_RASTER_DS (
     ID uuid,
@@ -115,25 +97,6 @@ create table SIMIDATA_RASTER_DS (
     primary key (ID)
 )^
 -- end SIMIDATA_RASTER_DS
--- begin FI_FEATURE_INFO
-create table FI_FEATURE_INFO (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    DISPLAY_TEMPLATE text not null,
-    SQL_QUERY text,
-    PY_MODULE_NAME varchar(100),
-    REMARKS text,
-    --
-    primary key (ID)
-)^
--- end FI_FEATURE_INFO
 -- begin SIMIPRODUCT_DATA_PRODUCT
 create table SIMIPRODUCT_DATA_PRODUCT (
     ID uuid,
@@ -208,23 +171,6 @@ create table SIMIDATA_MODEL_SCHEMA (
     primary key (ID)
 )^
 -- end SIMIDATA_MODEL_SCHEMA
--- begin PRODUCT_DATA_SET_VIEW
-create table PRODUCT_DATA_SET_VIEW (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    IDENTIFIER varchar(255) not null,
-    FEATURE_INFO_ID uuid,
-    --
-    primary key (ID)
-)^
--- end PRODUCT_DATA_SET_VIEW
 -- begin SIMIIAM_IDENTITY
 create table SIMIIAM_IDENTITY (
     ID uuid,
