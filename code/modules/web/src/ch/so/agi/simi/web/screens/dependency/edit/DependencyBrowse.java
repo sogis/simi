f297510f16dbb10c5bc7587ca7aaf4437d01d197
uncoyote <jeker.git@outlook.de>
2020-12-09 17:39:30 +0100

    Added screens for report

---
+++ code/modules/web/src/ch/so/agi/simi/web/screens/dependency/edit/DependencyBrowse.java
@@ -0,0 +1,38 @@
+package ch.so.agi.simi.web.screens.dependency.edit;
+
+import ch.so.agi.simi.entity.dependency.Report;
+import ch.so.agi.simi.entity.product.DataProduct;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.ScreenBuilders;
+import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.screen.*;
+import ch.so.agi.simi.entity.dependency.Dependency;
+
+import javax.inject.Inject;
+
+@UiController("simiDependency_Dependency.browse")
+@UiDescriptor("dependency-browse.xml")
+@LookupComponent("dependenciesTable")
+@LoadDataBeforeShow
+public class DependencyBrowse extends StandardLookup<Dependency> {
+    @Inject
+    private ScreenBuilders screenBuilders;
+    @Inject
+    private Table<Dependency> dependenciesTable;
+    @Inject
+    private Metadata metdata;
+
+    @Subscribe("createBtn.createReport")
+    public void onCreateBtnCreateReport(Action.ActionPerformedEvent event) {
+        Report report = metdata.create(Report.class);
+        showCreateEditorForDependency(report);
+    }
+
+    private void showCreateEditorForDependency(Dependency dependency) {
+        screenBuilders.editor(dependenciesTable)
+                .editEntity(dependency)
+                .build()
+                .show();
+    }
+}
