84bf8a0527a25e73631335a9dc529ba6ba258e17
uncoyote <jeker.git@outlook.de>
2021-01-04 21:25:26 +0100

    correct service name / docker compose exit code propagation

--- code/docker/await-db/await.sh
+++ code/docker/await-db/await.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in {1..20}; do
-    pg_isready -U postgres -h postgres
+for i in {1..2}; do
+    pg_isready -U postgres -h db # "... -h db" as the service name in the docker compose yml is "db"
     if [ $? -eq 0 ]
     then
       exit 0
