e09bdce709bf94f73bb50ad986961cbaa2e0758d
Oliver Jeker <uncoyote@users.noreply.github.com>
2021-01-04 21:32:26 +0100

    Update await.sh

--- code/docker/await-db/await.sh
+++ code/docker/await-db/await.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..2}; do
+for i in {1..20}; do
     pg_isready -U postgres -h db # "... -h db" as the service name in the docker compose yml is "db"
     if [ $? -eq 0 ]
     then
