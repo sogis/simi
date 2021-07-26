17d4f9eee937a1a69baf3ccfe3b6408253f5fdb5
uncoyote <jeker.git@outlook.de>
2021-07-26 14:26:48 +0200

    Set dispTemlate optional

--- webapp/modules/global/src/ch/so/agi/simi/entity/extended/FeatureInfo.java
+++ webapp/modules/global/src/ch/so/agi/simi/entity/extended/FeatureInfo.java
@@ -3,17 +3,16 @@ package ch.so.agi.simi.entity.extended;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
-import javax.validation.constraints.NotNull;
 
 @Entity(name = FeatureInfo.NAME)
 public class FeatureInfo extends Dependency {
 
     public static final String NAME = "simiExtended_FeatureInfo";
+
     private static final long serialVersionUID = 449117253137188881L;
 
     @Lob
     @Column(name = "DISPLAY_TEMPLATE")
-    @NotNull
     private String displayTemplate;
 
     @Lob
