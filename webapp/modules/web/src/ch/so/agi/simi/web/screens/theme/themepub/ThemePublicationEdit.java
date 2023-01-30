2bf4d1486a8ef8ded3cbb4ac081186a42fd63c75
uncoyote <jeker.git@outlook.de>
2023-01-30 16:35:37 +0100

    ThemePublication: Custom load for pubSubAreas as too large

--- webapp/modules/web/src/ch/so/agi/simi/web/screens/theme/themepub/ThemePublicationEdit.java
+++ webapp/modules/web/src/ch/so/agi/simi/web/screens/theme/themepub/ThemePublicationEdit.java
@@ -5,8 +5,13 @@ import ch.so.agi.simi.entity.product.DataProductDsv;
 import ch.so.agi.simi.entity.theme.Theme;
 import ch.so.agi.simi.entity.theme.ThemePublication;
 import ch.so.agi.simi.entity.theme.ThemePublication_TypeEnum;
+import ch.so.agi.simi.entity.theme.subarea.PublishedSubArea;
 import com.haulmont.cuba.core.entity.BaseUuidEntity;
 import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.components.Component;
 import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.components.HasValue;
@@ -15,6 +20,7 @@ import com.haulmont.cuba.gui.model.*;
 import com.haulmont.cuba.gui.screen.*;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.UUID;
 
 @UiController("simiTheme_ThemePublication.edit")
@@ -27,7 +33,14 @@ public class ThemePublicationEdit extends StandardEditor<ThemePublication> {
     private CollectionContainer<DataProductDsv> deepProductsDc;
 
     @Inject
+    private CollectionContainer<PublishedSubArea> pubAreasDc;
+
+    @Inject
     private DataComponents dataComponents;
+
+    @Inject
+    private Notifications notifications;
+
 
     @Subscribe(id = "themePublicationDl", target = Target.DATA_LOADER)
     public void onThemePublicationDlPostLoad(InstanceLoader.PostLoadEvent event) {
@@ -35,16 +48,40 @@ public class ThemePublicationEdit extends StandardEditor<ThemePublication> {
         if(e == null)
             return;
 
-        ThemePublication loadedTp = (ThemePublication)e;
+        UUID loadedTpId = ((BaseUuidEntity)e).getId();
 
+        loadProducts(loadedTpId);
+        loadPublishedSubAreas(loadedTpId);
+    }
+
+    private void loadProducts(UUID themePubId){
         CollectionLoader<DataProductDsv> loader = dataComponents.createCollectionLoader();
         loader.setQuery("select e from simiProduct_DataProductDsv e where e.themePublication = :tpId");
-        loader.setParameter("tpId", loadedTp.getId());
+        loader.setParameter("tpId", themePubId);
 
         loader.setContainer(deepProductsDc);
         loader.setDataContext(getScreenData().getDataContext());
 
         loader.load();
+    }
+
+    private void loadPublishedSubAreas(UUID themePubId){
+        CollectionLoader<PublishedSubArea> loader = dataComponents.createCollectionLoader();
+        loader.setQuery("select e from simiTheme_PublishedSubArea e where e.themePublication.id = :tpId");
+        loader.setParameter("tpId", themePubId);
+
+        loader.setContainer(pubAreasDc);
+        loader.setDataContext(getScreenData().getDataContext());
+
+        loader.setMaxResults(300);
+        loader.load();
+
+        if(loader.getContainer().getItems().size() == 200){
+            notifications.create(Notifications.NotificationType.TRAY)
+                    .withCaption("Teilladung publizierte Gebiete")
+                    .withDescription("Es wurden nur die ersten 200 Zeilen geladen. Vollständige Liste siehe Themen-Verwaltung -> Gebiet-Verknüpfungen")
+                    .show();
+        }
     }
 
     @Inject
